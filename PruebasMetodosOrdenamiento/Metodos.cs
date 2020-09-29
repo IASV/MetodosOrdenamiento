6821a6871e37ca5c99c121b2e7e95a4f9ceb34c3
IASV <iasuarez@unicesar.edu.co>
2020-09-29 09:35:08 -0500

    first comment

--- PruebasMetodosOrdenamiento/Metodos.cs
+++ PruebasMetodosOrdenamiento/Metodos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace PruebasMetodosOrdenamiento
 {
@@ -10,7 +11,7 @@ namespace PruebasMetodosOrdenamiento
 
         public static void RandomFill(int[] vs)
         {
-            for (int i = 0; i < vs.Length; i++)
+            for (int i = 0; i < vs.Length-1; i++)
             {
                 vs[i] = r.Next(100);
             }
@@ -25,7 +26,7 @@ namespace PruebasMetodosOrdenamiento
             Console.BackgroundColor = ConsoleColor.Black;
         }
 
-        static void printVector(int[] vs)
+        static void printvs(int[] vs)
         {
             for (int i = 0; i < vs.GetLength(0); i++)
             {
@@ -33,7 +34,7 @@ namespace PruebasMetodosOrdenamiento
             }
         }
 
-        static void printVectorColor(int[] vs, int index1, int index2)
+        static void printvsColor(int[] vs, int index1, int index2)
         {
             for (int i = 0; i < vs.GetLength(0); i++)
             {
@@ -43,51 +44,56 @@ namespace PruebasMetodosOrdenamiento
             }
         }
 
-        public static void Burbuja(int[] vs)
+        public static int Burbuja(int[] vs)
         {
             int cont = 0;
-            for (int i = 0; i < vs.Length -1; i++)
+            for (int i = 0; i < vs.Length - 1; i++)
             {
-                for (int j = 0; j < vs.Length; j++)
+                for (int j = i; j < vs.Length; j++)
                 {
-                    cont += 1;
+                    cont++;
                     if (vs[i] < vs[j])
                     {
                         int temp = vs[i];
                         vs[i] = vs[j];
                         vs[j] = temp;
 
-                        //printVectorColor(vs, i, j);
+                        //printvsColor(vs, i, j);
                     }
                     else
                     {
-                        //printVector(vs);
+                        //printvs(vs);
                     }
                 }
             }
-            Console.WriteLine("\titeraciones: " + cont);
+
+            return cont;
         }
-        public void InsercionDirecta(int[] vs)
+        public static int InsercionDirecta(int[] vs)
         {
+            int cont = 0;
             int auxili;
             int j;
             for (int i = 0; i < vs.Length; i++)
-            {   
+            {
                 auxili = vs[i];
                 j = i - 1;
                 while (j >= 0 && vs[j] > auxili)
                 {
                     vs[j + 1] = vs[j];
                     j--;
+                    cont += 1;
                 }
                 vs[j + 1] = auxili;
+                
             }
+            return cont;
         }
 
 
-        public void InsercionBinaria(int[] vs)
+        public static int InsercionBinaria(int[] vs)
         {
-            int auxiliar, q, izqui, dere, j=0;
+            int auxiliar, q, izqui, dere, j, cont = 0;
             for (int i = 0; i < vs.Length; i++)
             {
                 auxiliar = vs[i];
@@ -109,13 +115,200 @@ namespace PruebasMetodosOrdenamiento
                     {
                         vs[j + 1] = vs[j];
                         j = j - 1;
+                        cont++;
                     }
                     vs[izqui] = auxiliar;
                 }
             }
+            return cont;
+        }
+
+        public static int ShellSort(int[] vs)
+        {
+            int cont = 0;
+            int salto = 0;
+            int sw = 0;
+            int auxi = 0;
+            int e = 0;
+            salto = vs.Length / 2;
+            while (salto > 0)
+            {
+                sw = 1;
+                while (sw != 0)
+                {
+                    sw = 0;
+                    e = 1;
+                    while (e <= (vs.Length - salto))
+                    {
+                        if (vs[e - 1] > vs[(e - 1) + salto])
+                        {
+                            auxi = vs[(e - 1) + salto];
+                            vs[(e - 1) + salto] = vs[e - 1];
+                            vs[(e - 1)] = auxi;
+                            sw = 1;
+                            cont++;
+                        }
+                        e++;
+                    }
+                }
+                salto = salto / 2;
+            }
+            return cont;
+        }
+
+        public static int quicksort(int[] vs, int primero, int ultimo)
+        {
+            int cont = 0;
+            int i, j, central;
+            double pivote;
+            central = (primero + ultimo) / 2;
+            pivote = vs[central];
+            i = primero;
+            j = ultimo;
+            do
+            {
+                while (vs[i] < pivote) i++;
+                while (vs[j] > pivote) j--;
+                if (i <= j)
+                {
+                    int temp;
+                    temp = vs[i];
+                    vs[i] = vs[j];
+                    vs[j] = temp;
+                    i++;
+                    j--;
+                }
+                cont++;
+            } while (i <= j);
+
+            if (primero < j)
+            {
+                quicksort(vs, primero, j);
+            }
+            if (i < ultimo)
+            {
+                quicksort(vs, i, ultimo);
+            }
+            return cont;
+        }
+
+        public static int ShakerSort(int[] vs)
+        {
+            int cont = 0;
+            int n = vs.Length;
+            int izq = 1;
+            int k = n;
+            int aux;
+            int der = n-1;
+
+            do
+            {
+                for (int i = der; i > izq; i--)
+                {
+
+                    if (vs[i - 1] >= vs[i])
+                    {
+                        aux = vs[i - 1];
+                        vs[i - 1] = vs[i];
+                        vs[i] = aux;
+                        k = i;
+                    }
+                    cont++;
+                }
+                izq = k + 1;
+                for (int i = izq; i <= der; i++)
+                {
+                    if (vs[i - 1] >= vs[i])
+                    {
+                        aux = vs[i - 1];
+                        vs[i - 1] = vs[i];
+                        vs[i] = aux;
+                        k = 1;
+                    }
+                    cont++;
+                }
+                der = k - 1;
+            }
+            while (der >= izq);
+
+            return cont;
         }
 
 
+        public static int MergeSort(int[] x)
+        {
+            int cont = 0;
+            MergeSort(x, 0, x.Length - 1);
+            return cont;
+        }
 
+        static private void MergeSort(int[] x, int desde, int hasta)
+        {
+            //Condicion de parada
+            if (desde == hasta)
+                return;
+
+            //Calculo la mitad del array
+            int mitad = (desde + hasta) / 2;
+
+            //Voy a ordenar recursivamente la primera mitad
+            //y luego la segunda
+            MergeSort(x, desde, mitad);
+            MergeSort(x, mitad + 1, hasta);
+
+            //Mezclo las dos mitades ordenadas
+            int[] aux = Merge(x, desde, mitad, mitad + 1, hasta);
+            Array.Copy(aux, 0, x, desde, aux.Length);
+        }
+
+        //Método que mezcla las dos mitades ordenadas
+        static private int[] Merge(int[] x, int desde1, int hasta1, int desde2, int hasta2)
+        {
+            int a = desde1;
+            int b = desde2;
+            int[] result = new int[hasta1 - desde1 + hasta2 - desde2 + 2];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                
+                if (b != x.Length)
+                {
+                    if (a > hasta1 && b <= hasta2)
+                    {
+                        result[i] = x[b];
+                        b++;
+                    }
+                    if (b > hasta2 && a <= hasta1)
+                    {
+                        result[i] = x[a];
+                        a++;
+                    }
+                    if (a <= hasta1 && b <= hasta2)
+                    {
+                        if (x[b] <= x[a])
+                        {
+                            result[i] = x[b];
+                            b++;
+                        }
+                        else
+                        {
+                            result[i] = x[a];
+                            a++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (a <= hasta1)
+                    {
+                        result[i] = x[a];
+                        a++;
+                    }
+                }
+            }
+            return result;
+        }
     }
+
 }
+
--- PruebasMetodosOrdenamiento/Program.cs
+++ PruebasMetodosOrdenamiento/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net.WebSockets;
 
 namespace PruebasMetodosOrdenamiento
 {
@@ -6,25 +8,88 @@ namespace PruebasMetodosOrdenamiento
     {
         static void Main(string[] args)
         {
-            for (int i = 100; i < 100000; i += 100)
+            StreamWriter sw = new StreamWriter("Datos.txt",true);
+            int cont = 0;
+            for (int j = 5; j < 6; j++)
             {
-                int[] values = new int[i];
+                switch (j)
+                {
+                    case 0:
+                        Console.WriteLine("\n----------Metodo Burbuja----------\n" + "Data".PadRight(8) + "Time".PadRight(19) + "Iterations".PadRight(12));
+                        sw.WriteLine("\n----------Metodo Burbuja----------\n");
+                        break;
+                    case 1:
+                        Console.WriteLine("\n----------Metodo Inserción Directa----------\n" + "Data".PadRight(8) + "Time".PadRight(19) + "Iterations".PadRight(12));
+                        sw.WriteLine("\n----------Metodo Inserción Directa----------\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("\n----------Metodo Inserción Binaria----------\n" + "Data".PadRight(8) + "Time".PadRight(19) + "Iterations".PadRight(12));
+                        sw.WriteLine("\n----------Metodo Inserción Binaria----------\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("\n----------Metodo Shell Sort----------\n" + "Data".PadRight(8) + "Time".PadRight(19) + "Iterations".PadRight(12));
+                        sw.WriteLine("\n----------Metodo Shell Sort----------\n");
+                        break;
+                    case 4:
+                        Console.WriteLine("\n----------Metodo Quick Sort----------\n" + "Data".PadRight(8) + "Time".PadRight(19) + "Iterations".PadRight(12));
+                        sw.WriteLine("\n----------Metodo Qick Sort----------\n");
+                        break;
+                    case 5:
+                        Console.WriteLine("\n----------Metodo Shaker Sort----------\n" + "Data".PadRight(8) + "Time".PadRight(19) + "Iterations".PadRight(12));
+                        sw.WriteLine("\n----------Metodo Shaker Sort----------\n");
+                        break;
+                    case 6:
+                        Console.WriteLine("\n----------Metodo Merge Sort----------\n" + "Data".PadRight(8) + "Time".PadRight(19) + "Iterations".PadRight(12));
+                        sw.WriteLine("\n----------Metodo Merge Sort----------\n");
+                        break;
 
-                Metodos.RandomFill(values);
-                //para el contador e imprime el resultado:
-                DateTime tiempo1 = DateTime.Now;
+                }
+                for (int i = 0; i < 11; i += 1)
+                {
+                    int[] values = new int[i];
 
-                Metodos.Burbuja(values);
-                DateTime tiempo2 = DateTime.Now;
+                    Metodos.RandomFill(values);
+                    //para el contador e imprime el resultado:
+                    DateTime tiempo1 = DateTime.Now;
 
-                TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
-                Console.Write("Data: " + i.ToString());
-                Console.Write("\tTIEMPO: " + total.ToString());
+                    switch (j)
+                    {
+                        case 0:
+                            cont = Metodos.Burbuja(values);
+                            break;
+                        case 1:
+                            cont = Metodos.InsercionDirecta(values);
+                            break;
+                        case 2:
+                            cont = Metodos.InsercionBinaria(values);
+                            break;
+                        case 3:
+                            cont = Metodos.ShellSort(values);
+                            break;
+                        case 4:
+                            cont = Metodos.quicksort(values, 0, values.Length -1);
+                            break;
+                        case 5:
+                            cont = Metodos.ShakerSort(values);
+                            break;
+                        case 6:
+                            cont = Metodos.MergeSort(values);
+                            break;
+
+                    }
+
+                    DateTime tiempo2 = DateTime.Now;
+
+                    TimeSpan total = new TimeSpan(tiempo2.Ticks - tiempo1.Ticks);
+
+                    Console.WriteLine(i.ToString().PadRight(6) + "| " + total.ToString().PadRight(17) + "| " + cont.ToString().PadRight(10));
+                    sw.WriteLine(i.ToString() + ", " + total.ToString() + ", " + cont.ToString());
+
+                }
+
             }
 
-
-
-
+            sw.Close();
 
         }
     }
